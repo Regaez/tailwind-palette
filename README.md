188a39b7bbdedf6ece6cf0721e6d2afd94a86fb5
Thomas Threadgold <tj.threadgold@gmail.com>
2022-04-11 07:49:47 +1200

    feat: create palette

---
+++ README.md
@@ -0,0 +1,22 @@
+# Tailwind Palette
+
+Palette file for [Aseprite](https://www.aseprite.org/), generated based off of the [TailwindCSS default palette](https://tailwindcss.com/docs/customizing-colors#default-color-palette).
+
+## Getting started
+
+To install:
+
+- Download/clone this repo.
+- Open Aseprite.
+- Click "Load Palette" from the color palette menu.
+- Navigate to project folder and select the `tailwind.gpl` file.
+
+To regenerate `.gpl` file yourself:
+
+- Install NodeJS LTS version.
+- `npm run install` command within the project directory to acquire dependencies.
+- `npm run build` to regenerate the `tailwind.gpl` file.
+
+## Example
+
+![Example palette in Aseprite](example.png)
---
+++ index.ts
@@ -0,0 +1,903 @@
+const TAILIND_PALETTE = [
+  {
+    name: "Slate_50",
+    hex: "#f8fafc",
+  },
+  {
+    name: "Slate_100",
+    hex: "#f1f5f9",
+  },
+  {
+    name: "Slate_200",
+    hex: "#e2e8f0",
+  },
+  {
+    name: "Slate_300",
+    hex: "#cbd5e1",
+  },
+  {
+    name: "Slate_400",
+    hex: "#94a3b8",
+  },
+  {
+    name: "Slate_500",
+    hex: "#64748b",
+  },
+  {
+    name: "Slate_600",
+    hex: "#475569",
+  },
+  {
+    name: "Slate_700",
+    hex: "#334155",
+  },
+  {
+    name: "Slate_800",
+    hex: "#1e293b",
+  },
+  {
+    name: "Slate_900",
+    hex: "#0f172a",
+  },
+  {
+    name: "Gray_50",
+    hex: "#f9fafb",
+  },
+  {
+    name: "Gray_100",
+    hex: "#f3f4f6",
+  },
+  {
+    name: "Gray_200",
+    hex: "#e5e7eb",
+  },
+  {
+    name: "Gray_300",
+    hex: "#d1d5db",
+  },
+  {
+    name: "Gray_400",
+    hex: "#9ca3af",
+  },
+  {
+    name: "Gray_500",
+    hex: "#6b7280",
+  },
+  {
+    name: "Gray_600",
+    hex: "#4b5563",
+  },
+  {
+    name: "Gray_700",
+    hex: "#374151",
+  },
+  {
+    name: "Gray_800",
+    hex: "#1f2937",
+  },
+  {
+    name: "Gray_900",
+    hex: "#111827",
+  },
+  {
+    name: "Zinc_50",
+    hex: "#fafafa",
+  },
+  {
+    name: "Zinc_100",
+    hex: "#f4f4f5",
+  },
+  {
+    name: "Zinc_200",
+    hex: "#e4e4e7",
+  },
+  {
+    name: "Zinc_300",
+    hex: "#d4d4d8",
+  },
+  {
+    name: "Zinc_400",
+    hex: "#a1a1aa",
+  },
+  {
+    name: "Zinc_500",
+    hex: "#71717a",
+  },
+  {
+    name: "Zinc_600",
+    hex: "#52525b",
+  },
+  {
+    name: "Zinc_700",
+    hex: "#3f3f46",
+  },
+  {
+    name: "Zinc_800",
+    hex: "#27272a",
+  },
+  {
+    name: "Zinc_900",
+    hex: "#18181b",
+  },
+  {
+    name: "Neutral_50",
+    hex: "#fafafa",
+  },
+  {
+    name: "Neutral_100",
+    hex: "#f5f5f5",
+  },
+  {
+    name: "Neutral_200",
+    hex: "#e5e5e5",
+  },
+  {
+    name: "Neutral_300",
+    hex: "#d4d4d4",
+  },
+  {
+    name: "Neutral_400",
+    hex: "#a3a3a3",
+  },
+  {
+    name: "Neutral_500",
+    hex: "#737373",
+  },
+  {
+    name: "Neutral_600",
+    hex: "#525252",
+  },
+  {
+    name: "Neutral_700",
+    hex: "#404040",
+  },
+  {
+    name: "Neutral_800",
+    hex: "#262626",
+  },
+  {
+    name: "Neutral_900",
+    hex: "#171717",
+  },
+  {
+    name: "Stone_50",
+    hex: "#fafaf9",
+  },
+  {
+    name: "Stone_100",
+    hex: "#f5f5f4",
+  },
+  {
+    name: "Stone_200",
+    hex: "#e7e5e4",
+  },
+  {
+    name: "Stone_300",
+    hex: "#d6d3d1",
+  },
+  {
+    name: "Stone_400",
+    hex: "#a8a29e",
+  },
+  {
+    name: "Stone_500",
+    hex: "#78716c",
+  },
+  {
+    name: "Stone_600",
+    hex: "#57534e",
+  },
+  {
+    name: "Stone_700",
+    hex: "#44403c",
+  },
+  {
+    name: "Stone_800",
+    hex: "#292524",
+  },
+  {
+    name: "Stone_900",
+    hex: "#1c1917",
+  },
+  {
+    name: "Red_50",
+    hex: "#fef2f2",
+  },
+  {
+    name: "Red_100",
+    hex: "#fee2e2",
+  },
+  {
+    name: "Red_200",
+    hex: "#fecaca",
+  },
+  {
+    name: "Red_300",
+    hex: "#fca5a5",
+  },
+  {
+    name: "Red_400",
+    hex: "#f87171",
+  },
+  {
+    name: "Red_500",
+    hex: "#ef4444",
+  },
+  {
+    name: "Red_600",
+    hex: "#dc2626",
+  },
+  {
+    name: "Red_700",
+    hex: "#b91c1c",
+  },
+  {
+    name: "Red_800",
+    hex: "#991b1b",
+  },
+  {
+    name: "Red_900",
+    hex: "#7f1d1d",
+  },
+  {
+    name: "Orange_50",
+    hex: "#fff7ed",
+  },
+  {
+    name: "Orange_100",
+    hex: "#ffedd5",
+  },
+  {
+    name: "Orange_200",
+    hex: "#fed7aa",
+  },
+  {
+    name: "Orange_300",
+    hex: "#fdba74",
+  },
+  {
+    name: "Orange_400",
+    hex: "#fb923c",
+  },
+  {
+    name: "Orange_500",
+    hex: "#f97316",
+  },
+  {
+    name: "Orange_600",
+    hex: "#ea580c",
+  },
+  {
+    name: "Orange_700",
+    hex: "#c2410c",
+  },
+  {
+    name: "Orange_800",
+    hex: "#9a3412",
+  },
+  {
+    name: "Orange_900",
+    hex: "#7c2d12",
+  },
+  {
+    name: "Amber_50",
+    hex: "#fffbeb",
+  },
+  {
+    name: "Amber_100",
+    hex: "#fef3c7",
+  },
+  {
+    name: "Amber_200",
+    hex: "#fde68a",
+  },
+  {
+    name: "Amber_300",
+    hex: "#fcd34d",
+  },
+  {
+    name: "Amber_400",
+    hex: "#fbbf24",
+  },
+  {
+    name: "Amber_500",
+    hex: "#f59e0b",
+  },
+  {
+    name: "Amber_600",
+    hex: "#d97706",
+  },
+  {
+    name: "Amber_700",
+    hex: "#b45309",
+  },
+  {
+    name: "Amber_800",
+    hex: "#92400e",
+  },
+  {
+    name: "Amber_900",
+    hex: "#78350f",
+  },
+  {
+    name: "Yellow_50",
+    hex: "#fefce8",
+  },
+  {
+    name: "Yellow_100",
+    hex: "#fef9c3",
+  },
+  {
+    name: "Yellow_200",
+    hex: "#fef08a",
+  },
+  {
+    name: "Yellow_300",
+    hex: "#fde047",
+  },
+  {
+    name: "Yellow_400",
+    hex: "#facc15",
+  },
+  {
+    name: "Yellow_500",
+    hex: "#eab308",
+  },
+  {
+    name: "Yellow_600",
+    hex: "#ca8a04",
+  },
+  {
+    name: "Yellow_700",
+    hex: "#a16207",
+  },
+  {
+    name: "Yellow_800",
+    hex: "#854d0e",
+  },
+  {
+    name: "Yellow_900",
+    hex: "#713f12",
+  },
+  {
+    name: "Lime_50",
+    hex: "#f7fee7",
+  },
+  {
+    name: "Lime_100",
+    hex: "#ecfccb",
+  },
+  {
+    name: "Lime_200",
+    hex: "#d9f99d",
+  },
+  {
+    name: "Lime_300",
+    hex: "#bef264",
+  },
+  {
+    name: "Lime_400",
+    hex: "#a3e635",
+  },
+  {
+    name: "Lime_500",
+    hex: "#84cc16",
+  },
+  {
+    name: "Lime_600",
+    hex: "#65a30d",
+  },
+  {
+    name: "Lime_700",
+    hex: "#4d7c0f",
+  },
+  {
+    name: "Lime_800",
+    hex: "#3f6212",
+  },
+  {
+    name: "Lime_900",
+    hex: "#365314",
+  },
+  {
+    name: "Green_50",
+    hex: "#f0fdf4",
+  },
+  {
+    name: "Green_100",
+    hex: "#dcfce7",
+  },
+  {
+    name: "Green_200",
+    hex: "#bbf7d0",
+  },
+  {
+    name: "Green_300",
+    hex: "#86efac",
+  },
+  {
+    name: "Green_400",
+    hex: "#4ade80",
+  },
+  {
+    name: "Green_500",
+    hex: "#22c55e",
+  },
+  {
+    name: "Green_600",
+    hex: "#16a34a",
+  },
+  {
+    name: "Green_700",
+    hex: "#15803d",
+  },
+  {
+    name: "Green_800",
+    hex: "#166534",
+  },
+  {
+    name: "Green_900",
+    hex: "#14532d",
+  },
+  {
+    name: "Emerald_50",
+    hex: "#ecfdf5",
+  },
+  {
+    name: "Emerald_100",
+    hex: "#d1fae5",
+  },
+  {
+    name: "Emerald_200",
+    hex: "#a7f3d0",
+  },
+  {
+    name: "Emerald_300",
+    hex: "#6ee7b7",
+  },
+  {
+    name: "Emerald_400",
+    hex: "#34d399",
+  },
+  {
+    name: "Emerald_500",
+    hex: "#10b981",
+  },
+  {
+    name: "Emerald_600",
+    hex: "#059669",
+  },
+  {
+    name: "Emerald_700",
+    hex: "#047857",
+  },
+  {
+    name: "Emerald_800",
+    hex: "#065f46",
+  },
+  {
+    name: "Emerald_900",
+    hex: "#064e3b",
+  },
+  {
+    name: "Teal_50",
+    hex: "#f0fdfa",
+  },
+  {
+    name: "Teal_100",
+    hex: "#ccfbf1",
+  },
+  {
+    name: "Teal_200",
+    hex: "#99f6e4",
+  },
+  {
+    name: "Teal_300",
+    hex: "#5eead4",
+  },
+  {
+    name: "Teal_400",
+    hex: "#2dd4bf",
+  },
+  {
+    name: "Teal_500",
+    hex: "#14b8a6",
+  },
+  {
+    name: "Teal_600",
+    hex: "#0d9488",
+  },
+  {
+    name: "Teal_700",
+    hex: "#0f766e",
+  },
+  {
+    name: "Teal_800",
+    hex: "#115e59",
+  },
+  {
+    name: "Teal_900",
+    hex: "#134e4a",
+  },
+  {
+    name: "Cyan_50",
+    hex: "#ecfeff",
+  },
+  {
+    name: "Cyan_100",
+    hex: "#cffafe",
+  },
+  {
+    name: "Cyan_200",
+    hex: "#a5f3fc",
+  },
+  {
+    name: "Cyan_300",
+    hex: "#67e8f9",
+  },
+  {
+    name: "Cyan_400",
+    hex: "#22d3ee",
+  },
+  {
+    name: "Cyan_500",
+    hex: "#06b6d4",
+  },
+  {
+    name: "Cyan_600",
+    hex: "#0891b2",
+  },
+  {
+    name: "Cyan_700",
+    hex: "#0e7490",
+  },
+  {
+    name: "Cyan_800",
+    hex: "#155e75",
+  },
+  {
+    name: "Cyan_900",
+    hex: "#164e63",
+  },
+  {
+    name: "Sky_50",
+    hex: "#f0f9ff",
+  },
+  {
+    name: "Sky_100",
+    hex: "#e0f2fe",
+  },
+  {
+    name: "Sky_200",
+    hex: "#bae6fd",
+  },
+  {
+    name: "Sky_300",
+    hex: "#7dd3fc",
+  },
+  {
+    name: "Sky_400",
+    hex: "#38bdf8",
+  },
+  {
+    name: "Sky_500",
+    hex: "#0ea5e9",
+  },
+  {
+    name: "Sky_600",
+    hex: "#0284c7",
+  },
+  {
+    name: "Sky_700",
+    hex: "#0369a1",
+  },
+  {
+    name: "Sky_800",
+    hex: "#075985",
+  },
+  {
+    name: "Sky_900",
+    hex: "#0c4a6e",
+  },
+  {
+    name: "Blue_50",
+    hex: "#eff6ff",
+  },
+  {
+    name: "Blue_100",
+    hex: "#dbeafe",
+  },
+  {
+    name: "Blue_200",
+    hex: "#bfdbfe",
+  },
+  {
+    name: "Blue_300",
+    hex: "#93c5fd",
+  },
+  {
+    name: "Blue_400",
+    hex: "#60a5fa",
+  },
+  {
+    name: "Blue_500",
+    hex: "#3b82f6",
+  },
+  {
+    name: "Blue_600",
+    hex: "#2563eb",
+  },
+  {
+    name: "Blue_700",
+    hex: "#1d4ed8",
+  },
+  {
+    name: "Blue_800",
+    hex: "#1e40af",
+  },
+  {
+    name: "Blue_900",
+    hex: "#1e3a8a",
+  },
+  {
+    name: "Indigo_50",
+    hex: "#eef2ff",
+  },
+  {
+    name: "Indigo_100",
+    hex: "#e0e7ff",
+  },
+  {
+    name: "Indigo_200",
+    hex: "#c7d2fe",
+  },
+  {
+    name: "Indigo_300",
+    hex: "#a5b4fc",
+  },
+  {
+    name: "Indigo_400",
+    hex: "#818cf8",
+  },
+  {
+    name: "Indigo_500",
+    hex: "#6366f1",
+  },
+  {
+    name: "Indigo_600",
+    hex: "#4f46e5",
+  },
+  {
+    name: "Indigo_700",
+    hex: "#4338ca",
+  },
+  {
+    name: "Indigo_800",
+    hex: "#3730a3",
+  },
+  {
+    name: "Indigo_900",
+    hex: "#312e81",
+  },
+  {
+    name: "Violet_50",
+    hex: "#f5f3ff",
+  },
+  {
+    name: "Violet_100",
+    hex: "#ede9fe",
+  },
+  {
+    name: "Violet_200",
+    hex: "#ddd6fe",
+  },
+  {
+    name: "Violet_300",
+    hex: "#c4b5fd",
+  },
+  {
+    name: "Violet_400",
+    hex: "#a78bfa",
+  },
+  {
+    name: "Violet_500",
+    hex: "#8b5cf6",
+  },
+  {
+    name: "Violet_600",
+    hex: "#7c3aed",
+  },
+  {
+    name: "Violet_700",
+    hex: "#6d28d9",
+  },
+  {
+    name: "Violet_800",
+    hex: "#5b21b6",
+  },
+  {
+    name: "Violet_900",
+    hex: "#4c1d95",
+  },
+  {
+    name: "Purple_50",
+    hex: "#faf5ff",
+  },
+  {
+    name: "Purple_100",
+    hex: "#f3e8ff",
+  },
+  {
+    name: "Purple_200",
+    hex: "#e9d5ff",
+  },
+  {
+    name: "Purple_300",
+    hex: "#d8b4fe",
+  },
+  {
+    name: "Purple_400",
+    hex: "#c084fc",
+  },
+  {
+    name: "Purple_500",
+    hex: "#a855f7",
+  },
+  {
+    name: "Purple_600",
+    hex: "#9333ea",
+  },
+  {
+    name: "Purple_700",
+    hex: "#7e22ce",
+  },
+  {
+    name: "Purple_800",
+    hex: "#6b21a8",
+  },
+  {
+    name: "Purple_900",
+    hex: "#581c87",
+  },
+  {
+    name: "Fuchsia_50",
+    hex: "#fdf4ff",
+  },
+  {
+    name: "Fuchsia_100",
+    hex: "#fae8ff",
+  },
+  {
+    name: "Fuchsia_200",
+    hex: "#f5d0fe",
+  },
+  {
+    name: "Fuchsia_300",
+    hex: "#f0abfc",
+  },
+  {
+    name: "Fuchsia_400",
+    hex: "#e879f9",
+  },
+  {
+    name: "Fuchsia_500",
+    hex: "#d946ef",
+  },
+  {
+    name: "Fuchsia_600",
+    hex: "#c026d3",
+  },
+  {
+    name: "Fuchsia_700",
+    hex: "#a21caf",
+  },
+  {
+    name: "Fuchsia_800",
+    hex: "#86198f",
+  },
+  {
+    name: "Fuchsia_900",
+    hex: "#701a75",
+  },
+  {
+    name: "Pink_50",
+    hex: "#fdf2f8",
+  },
+  {
+    name: "Pink_100",
+    hex: "#fce7f3",
+  },
+  {
+    name: "Pink_200",
+    hex: "#fbcfe8",
+  },
+  {
+    name: "Pink_300",
+    hex: "#f9a8d4",
+  },
+  {
+    name: "Pink_400",
+    hex: "#f472b6",
+  },
+  {
+    name: "Pink_500",
+    hex: "#ec4899",
+  },
+  {
+    name: "Pink_600",
+    hex: "#db2777",
+  },
+  {
+    name: "Pink_700",
+    hex: "#be185d",
+  },
+  {
+    name: "Pink_800",
+    hex: "#9d174d",
+  },
+  {
+    name: "Pink_900",
+    hex: "#831843",
+  },
+  {
+    name: "Rose_50",
+    hex: "#fff1f2",
+  },
+  {
+    name: "Rose_100",
+    hex: "#ffe4e6",
+  },
+  {
+    name: "Rose_200",
+    hex: "#fecdd3",
+  },
+  {
+    name: "Rose_300",
+    hex: "#fda4af",
+  },
+  {
+    name: "Rose_400",
+    hex: "#fb7185",
+  },
+  {
+    name: "Rose_500",
+    hex: "#f43f5e",
+  },
+  {
+    name: "Rose_600",
+    hex: "#e11d48",
+  },
+  {
+    name: "Rose_700",
+    hex: "#be123c",
+  },
+  {
+    name: "Rose_800",
+    hex: "#9f1239",
+  },
+  {
+    name: "Rose_900",
+    hex: "#881337",
+  }
+];
+
+const hexToRgb = (hex: string): { r: number, g: number, b: number } => {
+  const regex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
+  const [_, r = "0", g = "0", b = "0"] = regex.exec(hex) || [];
+  return {
+    r: parseInt(r, 16),
+    g: parseInt(g, 16),
+    b: parseInt(b, 16)
+  };
+}
+
+const fmt = (n: number): string => n.toString().padStart(3, " ");
+
+console.log(`GIMP Palette`);
+console.log(`Name: Tailwind`);
+console.log(`#`);
+
+for (const color of TAILIND_PALETTE) {
+  const { r, g, b } = hexToRgb(color.hex);
+  console.log(`${fmt(r)} ${fmt(g)} ${fmt(b)} ${color.name}`);
+};
